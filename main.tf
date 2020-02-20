054ba35e6484808995c59c28f8b28b756e515bb0
b0s00dg <b0s00dg@homeoffice.wal-mart.com>
2020-02-20 08:55:47 -0600

    moving to modules instead of explicit resource statements

--- main.tf
+++ main.tf
@@ -1,156 +1,59 @@
-/* Specifying a variable
-* variable "<var_name>" {
-*   default = "<value>"
-* }
-* Call it like ${var.<var_name>}
-* Substantiate it with -var '<var_name>=<value>' or from terraform.tfvars to assign variables from a file
-* Specifying a variable in a .tf file with no default and without passing anything to apply will result in it asking you for values
-*/
+# declare variables and defaults
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  subscription_id = "6fbc4b5d-b5eb-42c1-974d-a5d2bc5d64fd"
-  tenant_id       = "2baef15b-b8de-423f-9d8a-46f3686d8848"
+variable "vm_name" {
+  default     = "nillagoober-prime"
+  description = "VM name, up to 15 characters, numbers and letters, no special characters except hyphen -"
 }
 
-# Create a resource group if it doesn’t exist
-resource "azurerm_resource_group" "nillagoober_tf_rg" {
-  name     = "nillagoober_rg"
-  location = "westus"
-
-  tags = {
-    environment = "Nillagoober Dev"
-  }
+variable "admin_username"{
+  default     = "azroot"
+  description = "Admin user name for the virtual machine"
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "nillagoober_virt_net" {
-  name                = "nillagoober_vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "westus"
-  resource_group_name = azurerm_resource_group.nillagoober_tf_rg.name
-
-  tags = {
-    environment = "Nillagoober Dev"    
-  }
+variable "location" {
+  default     = "westus"
+  description = "Azure region"
 }
 
-# Create subnet
-resource "azurerm_subnet" "nillagoober_tf_subnet" {
-  name                 = "nillagoober_subnet"
-  resource_group_name  = azurerm_resource_group.nillagoober_tf_rg.name
-  virtual_network_name = azurerm_virtual_network.nillagoober_virt_net.name
-  address_prefix        = "10.0.1.0/24"
+variable "environment" {
+  default = "dev"
 }
-
-# Create public IPs
-resource "azurerm_public_ip" "nillagoober_tf_pub_ip" {
-  name                = "nillagoober_public_ip"
-  location            = "westus"
-  resource_group_name = azurerm_resource_group.nillagoober_tf_rg.name
-  allocation_method   = "Dynamic"
-
-  tags = {
-    environment = "Nillagoober Dev"    
+variable "vm_size" {
+  default = {
+    "dev"  = "Standard_B1ls"
+    "prod" = "Standard_B1ls"
   }
 }
+# end vars
 
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "nillagoober_tf_nsg" {
-  name                = "nillagoober_nsg"
-  location            = "westus"
-  resource_group_name = azurerm_resource_group.nillagoober_tf_rg.name
-    
-  security_rule {
-    name                      = "SSH"
-    priority                  = 1001
-    direction                 = "Inbound"
-    access                    = "Allow"
-    protocol                  = "Tcp"
-    source_port_range         = "*"
-    destination_port_range    = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"    
-  }
-
-  tags = {
-    environment = "Nillagoober Dev"    
-  }
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "nillagoob_tf_rg"
+  location = var.location
 }
 
-# Create network interface
-resource "azurerm_network_interface" "nillagoober_tf_nic" {
-  name                      = "nillagoober_nic"
-  location                  = "westus"
-  resource_group_name       = azurerm_resource_group.nillagoober_tf_rg.name
-  network_security_group_id = azurerm_network_security_group.nillagoober_tf_nsg.id
-
-  ip_configuration {
-    name                          = "nillagoober_nic_config"
-    subnet_id                     = azurerm_subnet.nillagoober_tf_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.nillagoober_tf_pub_ip.id
-  }
-
-  tags = {
-    environment = "Nillagoober Dev"    
-  }
+# Use the network module to create a vnet and subnet
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "~> 2.0"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = "10.0.0.0/16"
+  subnet_names        = ["nillagoob_subnet"]
+  subnet_prefixes      = ["10.0.1.0/24"]
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.nillagoober_tf_rg.name
-  }
-  byte_length = 8
-}
-
-# Create storage account for boot diagnostics
-resource "azurerm_storage_account" "nillagoober_storage_acc" {
-  name                     = "diag${random_id.randomId.hex}"
-  resource_group_name      = azurerm_resource_group.nillagoober_tf_rg.name
-  location                 = "westus"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "Nillagoober Dev"    
-  }
-}
-
-# Create virtual machine
-resource "azurerm_virtual_machine" "nillagoober_vm" {
-  name                  = "nillagoob_prime"
-  location              = "westus"
-  resource_group_name   = azurerm_resource_group.nillagoober_tf_rg.name
-  network_interface_ids = [azurerm_network_interface.nillagoober_tf_nic.id]
-  vm_size               = "Standard_B1s"
-
-  storage_os_disk {
-    name              = "OsDisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7.5"
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "nillagoob"
-    admin_username = "azroot"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/azroot/.ssh/authorized_keys"
-      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDg9L8FqM+j30y7NGrY05/V8c4e9WfVFVrWn3WtLTxfZ5vgAFVm8OgnsEWMQ3gZgVQDUpGqwIV50nth3WL2LQR6/LGZJwdpu2Nr8g03Byrid/4G1D2uQqSH7AJqmMtnVukYtha5qUvyjYYULeHlsUIY92apRyoc9chY/6mWMNu+uNF8VXxLSO5ujBDjwIE0D3RXx81/TtfJiefgI3k9B0tWAjcXJPL5SgXWE1xi/PJHFRuB7mJShfLE+ZJOxF8WMJwNkUJv5BOOGayXUuxuC/+N5Ggy93EfQN7EBFuVwSz9A353CGFz733NklDpbrsQSl98FXH99slEupVT03xf3Jth b0s00dg@yeeticusprime"    
-    }
-  }
+# Use the compute module to create the VM
+module "compute" {
+  source              = "Azure/compute/azurerm"
+  version             = "~> 1.3"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  vm_hostname         = var.vm_name
+  vnet_subnet_id      = element(module.network.vnet_subnets, 0)
+  admin_username      = var.admin_username
+  remote_port         = "22"
+  vm_os_simple        = "UbuntuServer"
+  vm_size             = var.vm_size[var.environment]
+  public_ip_dns       = ["nillagoob-dns"]
 }
